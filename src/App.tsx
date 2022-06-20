f45b8a1322116567a679374ab131a3a7a6e43cf7
Thomas <schmiddla@gmail.com>
2022-06-21 01:04:04 +0200

    themecontext

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,57 @@
-import { useState } from 'react';
+import { createContext, useContext, useState } from 'react';
+import { NavLink, Route, Routes, useNavigate } from 'react-router-dom';
 import './App.scss';
 import './tailwind.scss';
 
-const themes = ['standard', 'red', 'green', 'new'];
+const themes: string[] = ['standard', 'red', 'green', 'dark', 'light'];
+
+interface IThemeContext {
+  theme: string;
+  setTheme: (theme: string) => void;
+}
+
+const defaultThemeContext: IThemeContext = {
+  theme: themes[2],
+  setTheme: (theme: string): void => {},
+};
+
+// const ThemeContext = createContext<IThemeContext | null>(null);
+const ThemeContext = createContext<IThemeContext>(defaultThemeContext);
+
+const ThemeContextProvider = ({ children }: { children: React.ReactNode }) => {
+  const [theme, setTheme] = useState<string>(defaultThemeContext.theme);
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
 
 function App() {
-  const [clickCounter, setClickCounter] = useState<number>(0);
-  const [theme, setTheme] = useState<string>(themes[0]);
+  return (
+    <ThemeContextProvider>
+      <Routes>
+        <Route path={`/`} element={<Page />} />
+        <Route path={`/page2`} element={<PageTwo />} />
+      </Routes>
+    </ThemeContextProvider>
+  );
+}
 
-  const handleClick = () => {
+function Page() {
+  const [clickCounter, setClickCounter] = useState<number>(0);
+  // const [theme, setTheme] = useState<string>(themes[0]);
+
+  const { theme, setTheme } = useContext(ThemeContext);
+  // const { theme, setTheme } = useContext(ThemeContext) || defaultThemeContext;
+
+  const handleClick = (): void => {
     setClickCounter(clickCounter + 1);
     setTheme(
       themes.indexOf(theme) === themes.length - 1
         ? themes[0]
         : themes[themes.indexOf(theme) + 1]
     );
-    // theme === 'dark'
-    //   ? 'green'
-    //   : theme === 'green'
-    //   ? 'red'
-    //   : theme === 'red'
-    //   ? 'light'
-    //   : 'dark'
-    // );
   };
 
   return (
@@ -30,7 +59,7 @@ function App() {
       <div className="pt-4 uppercase">theme: {theme}</div>
 
       <div className="p-4 m-8 rounded-xl bg-skin-fill bg-opacity-50">
-        <div className="p-4 text-skin-base text-5xl">This is a text</div>
+        <div className="p-4 text-skin-base text-5xl">Home</div>
         {/* <button
           onClick={handleClick}
           className="btn tablet:text-lg desktop:text-2xl"
@@ -51,7 +80,64 @@ function App() {
             second button
           </button>
         </div>
-        <div className="pt-2">{clickCounter}</div>
+        <div className="pt-2 text-skin-base">{clickCounter}</div>
+        <NavLink
+          className={'link text-skin-base inline-block mt-4'}
+          to={'/page2'}
+        >
+          to Page 2
+        </NavLink>
+      </div>
+    </div>
+  );
+}
+
+function PageTwo() {
+  const [clickCounter, setClickCounter] = useState<number>(0);
+  // const [theme, setTheme] = useState<string>(themes[0]);
+
+  const { theme, setTheme } = useContext(ThemeContext);
+  // const { theme, setTheme } = useContext(ThemeContext) || defaultThemeContext;
+
+  const handleClick = (): void => {
+    setClickCounter(clickCounter + 1);
+    setTheme(
+      themes.indexOf(theme) === themes.length - 1
+        ? themes[0]
+        : themes[themes.indexOf(theme) + 1]
+    );
+  };
+
+  return (
+    <div className={`App ${theme}`}>
+      <div className="pt-4 uppercase">theme: {theme}</div>
+
+      <div className="p-4 m-8 rounded-xl bg-skin-fill bg-opacity-50">
+        <div className="p-4 text-skin-base text-5xl">Page 2</div>
+        {/* <button
+          onClick={handleClick}
+          className="btn tablet:text-lg desktop:text-2xl"
+        >
+          Hello daisyUI
+        </button> */}
+        <div className="flex justify-center gap-6 flex-col tablet:flex-row">
+          <button
+            onClick={handleClick}
+            className="btn text-skin-inverted bg-skin-button-accent hover:bg-skin-button-accent-hover border-0"
+          >
+            first button Page2
+          </button>
+          <button
+            onClick={handleClick}
+            className="btn text-skin-base bg-skin-button-muted hover:bg-opacity-20 border-0"
+          >
+            second button Page2
+          </button>
+        </div>
+        <div className="pt-2 text-skin-base">{clickCounter}</div>
+        <NavLink className={'link text-skin-base inline-block mt-4'} to={'/'}>
+          to Page 1
+        </NavLink>
       </div>
     </div>
   );
